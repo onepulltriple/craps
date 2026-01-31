e2d899e7938a62e7b9f12de2ce7b8b8717c877a2
Chase Helgenlechner <chase@helgenlechner.com>
2026-01-31 12:10:11 +0100

    Create first draft of Bet base class.

---
+++ CrapsLibrary/Bet.cs
@@ -0,0 +1,49 @@
+﻿using System.Numerics;
+
+namespace CrapsLibrary
+{
+    internal class Bet
+    {
+        public string betName;
+        public int payoutNumerator;
+        public int payoutDenominator;
+        public bool isWorking;
+
+        public Bet(string betName, int payoutNumerator, int payoutDenominator)
+        {
+            this.betName = betName;
+            this.payoutNumerator = payoutNumerator;
+            this.payoutDenominator = payoutDenominator;
+            this.StartWorking(); // activates bet upon creation
+        }
+
+        public void StartWorking()
+        {
+            this.isWorking = true;
+        }
+
+        public void QuitWorking()
+        {
+            this.isWorking = false;
+        }
+
+        public bool AmountIsAllowed(int amount)
+        {
+            if (amount <= 0)
+            {
+                return false;
+                // error message: amount must be greater than zero!
+            }
+
+            if (amount % payoutDenominator != 0)
+            {
+                return false;
+                // error message: amount must be a multiple of payoutDenominator
+                // e.g. payoutDenominator * 1, payoutDenominator * 2, payoutDenominator * 3 ...
+            }
+
+            return true;
+        }
+
+    }
+}

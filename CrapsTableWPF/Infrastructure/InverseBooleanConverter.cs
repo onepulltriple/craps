dde8e590a707a087c965d6bceb183593e555e809
Chase Helgenlechner <chase@helgenlechner.com>
2026-05-21 17:42:23 +0200

    Added behavior to allow the user to "click on an empty slot" to add a player instead of a button that is always visible.

---
+++ CrapsTableWPF/Infrastructure/InverseBooleanConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace CrapsTableWPF.Infrastructure
+{
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+    }
+}

fcb85ca40d3d0bcd83049bf1810a95ad3e153d4f
Chase Helgenlechner <chase@helgenlechner.com>
2026-06-16 14:36:35 +0200

    Continued developing and testing the CreateBetWPF() method. Added an initial hover-over styling for the bet paths.

--- CrapsTableWPF/ViewModels/HardWayBetViewModel.cs
+++ CrapsTableWPF/ViewModels/HardWayBetViewModel.cs
@@ -12,11 +12,14 @@ namespace CrapsTableWPF.ViewModels
 
         private readonly CrapsTable crapsTable;
 
+        private readonly betType betType;
+
         public ICommand CreateBetCommand { get; }
 
         public HardWayBetViewModel(CrapsTable crapsTable, betType betType)
         {
             this.crapsTable = crapsTable;
+            this.betType = betType;
 
             this.BetSlotViewModels = new ObservableCollection<BetSlotViewModel>(
                 crapsTable.Slots.Select(
@@ -35,14 +38,35 @@ namespace CrapsTableWPF.ViewModels
 
         public void CreateBetWPF() // TODO send to a base class and make CreateOrUpdate()
         {
+            // determine which player is trying to bet
             Result<Player> currentPlayer = crapsTable.GetCurrentPlayer();
 
+            // error message if current player retrieval fails
             if (!currentPlayer.Success)
             {
                 crapsTable.gameEventFeed.AddMultiLine(currentPlayer);
                 return;
             }
 
+            // check if the player is allowed to place this bet
+            Result<bool> isBetAllowed = BetFactory.CheckIfCreateBetAllowed(this.crapsTable, currentPlayer.Value, this.betType);
+
+            // error message if the bet is not allowed
+            if (!isBetAllowed.Success)
+            {
+                crapsTable.gameEventFeed.AddMultiLine(isBetAllowed);
+                return;
+            }
+
+            // create bet for this player
+            Result<Bet> createBetResult = BetFactory.CreateBet(this.crapsTable, currentPlayer.Value, this.betType, 0);
+
+            // error message if the bet creation failed
+            if (!createBetResult.Success)
+            {
+                crapsTable.gameEventFeed.AddMultiLine(createBetResult);
+                return;
+            }
 
 
         }
